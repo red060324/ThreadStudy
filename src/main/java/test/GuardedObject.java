14d512e0f8f400737c568a67620e192de2185c4c
Red <148629701+red060324@users.noreply.github.com>
2025-05-02 16:08:03 +0800

    设计模式：保护性暂停（增加超时时间等待）

--- src/main/java/test/GuardedObject.java
+++ src/main/java/test/GuardedObject.java
@@ -5,14 +5,27 @@ public class GuardedObject {
     private Object response;
 
     //获取结果
-    public Object get() {
+    //timeOut 表示要等多久
+    public Object get(long timeout) {
         synchronized (this){
+            //开始时间
+            long begin = System.currentTimeMillis();
+            //经历的时间
+            long passedTime = 0;
             while (response == null){
+                //这一轮循环应该等待的时间
+                long waitTime = timeout - passedTime;
+                if (waitTime <= 0){
+                    break;
+                }
                 try {
-                    this.wait();
+                    this.wait(waitTime);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+
+                //求得经历时间
+                passedTime = System.currentTimeMillis() - begin;
             }
             return response;
         }
